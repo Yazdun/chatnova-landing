d8276e31415d54c0ac216425d30909e2b3d69eeb
Yazdun <yazduntube@gmail.com>
2023-10-20 03:41:45 +0330

    commit

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,14 +5,12 @@ import React from 'react'
 export const Navigation = () => {
   return (
     <div className="py-2 fixed top-0 left-0 z-50 backdrop-blur-3xl right-0 bg-blue-900/30 text-blue-400">
-      <nav className="container">
+      <nav className="container text-sm md:text-base">
         <a href="https://github.com/Kyler18/chat-nova" target="_blank">
-          <div className="flex gap-1 items-center w-full justify-center">
-            Chat Nova is now fully
-            <span className="hidden md:flex">free and</span> open-sourced,{' '}
-            <span className="underline">Try it out!</span>
-            🎉
-          </div>
+          Chat Nova is now fully
+          <span className="hidden md:inline">free and</span> open-sourced,{' '}
+          <span className="underline">Try it out!</span>
+          🎉
         </a>
       </nav>
     </div>
